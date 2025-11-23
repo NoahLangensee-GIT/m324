bd19423a1335e56fe0852ddbc255c19525a06828
Noah Langensee <noah.langensee@sluz.ch>
2025-11-23 21:29:54 +0100

    ex2 linting

--- linting/ex1/src/main.py
+++ linting/ex1/src/main.py
@@ -1,6 +1,16 @@
-import House
+import os
+from house import House
 
-villa = House()
+username = os.getenv("username")
+password = os.getenv("password")
 
-villa.SetName("Neverland")
-villa.GetName()
+if not username or not password:
+	exit(1)
+
+input_username = input("Username: ")
+input_password = input("Password: ")
+
+if username == input_username and password == input_password:
+	villa = House()
+	villa.SetName("Neverland")
+	villa.GetName()
